37660a03ea379b1504ea16d8da1c9afbc2ece878
lililian <musasaliem93@gmail.com>
2023-10-24 09:56:43 +0700

    add Collectioncontroller, get collection, collection blade php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SearchController;
+use App\Http\Controllers\CollectionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,4 @@ use App\Http\Controllers\SearchController;
 
 Route::get('/',[HomeController::class,'index'])->name('home');
 Route::get('/search',[SearchController::class,'index'])->name('search');
+Route::get('/collection',[CollectionController::class,'index'])->name('collection');
